e7843c859dceae43ebc04f7342d2742b2b4d73ec
Fazlul Shahriar <fshahriar@gmail.com>
2010-07-17 22:17:56 -0400

    add test for FindMachine

--- netrc/netrc_test.go
+++ netrc/netrc_test.go
@@ -8,6 +8,15 @@ import (
 	"testing"
 )
 
+var expectedMach = []*Machine{
+	&Machine{"mail.google.com", "joe@gmail.com", "somethingSecret", "gmail"},
+	&Machine{"ray", "demo", "mypassword", ""},
+	&Machine{"", "anonymous", "joe@example.com", ""},
+}
+var expectedMac = Macros{
+	"allput": "put src/*",
+}
+
 func eqMach(a *Machine, b *Machine) bool {
 	return a.Name == b.Name &&
 		a.Login == b.Login &&
@@ -21,23 +30,33 @@ func TestParse(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	expectedMach := []*Machine{
-		&Machine{"mail.google.com", "joe@gmail.com", "somethingSecret", "gmail"},
-		&Machine{"ray", "demo", "mypassword", ""},
-		&Machine{"", "anonymous", "joe@example.com", ""},
-	}
 	for i, e := range expectedMach {
 		if !eqMach(e, mach[i]) {
 			t.Errorf("bad machine; expected %v, got %v\n", e, mach[i])
 		}
 	}
 
-	expectedMac := Macros{
-		"allput": "put src/*",
-	}
 	for k, v := range expectedMac {
 		if v != mac[k] {
 			t.Errorf("bad macro for %s; expected %s, got %s\n", k, v, mac[k])
 		}
+	}
+}
+
+func TestFindMachine(t *testing.T) {
+	m, err := FindMachine("example.netrc", "ray")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !eqMach(m, expectedMach[1]) {
+		t.Errorf("bad machine; expected %v, got %v\n", expectedMach[1], m)
+	}
+
+	m, err = FindMachine("example.netrc", "non.existent")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !eqMach(m, expectedMach[2]) {
+		t.Errorf("bad machine; expected %v, got %v\n", expectedMach[2], m)
 	}
 }
